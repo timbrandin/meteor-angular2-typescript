3cc6c9d9dd942486751a681702c0a339b5142fd5
Tim Brandin <tim.brandin@gmail.com>
2015-08-07 16:58:19 +0200

    Version bump.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'netanelgilad:angular2-typescript',
-  version: '0.0.3',
+  name: 'timbrandin:angular2-typescript',
+  version: '1.5.3_1',
   // Brief, one-line summary of the package.
   summary: 'Compile Angular2 code using Typescript compiler.',
   // URL to the Git repository containing the source code for this package.
-  git: 'http://github.com/netanelgilad/meteor-angular2-typescript',
+  git: 'http://github.com/timbrandin/meteor-angular2-typescript',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
